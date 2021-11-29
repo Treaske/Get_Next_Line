cae58789f6aeace53c6c7b174b8d890935051f8d
Treaske <78087802+Treaske@users.noreply.github.com>
2021-11-29 20:33:02 +0100

    Create get_next_line.h

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-blas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/29 18:56:18 by ade-blas          #+#    #+#             */
+/*   Updated: 2021/11/29 18:59:51 by ade-blas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+#include <fcntl.h>
+
+char	*get_next_line(int fd);
+
+#endif
