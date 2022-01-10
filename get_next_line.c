b4e55d459ec4aa2d29c7c8b6d61be00f92d1ee3b
Treaske <78087802+Treaske@users.noreply.github.com>
2022-01-10 19:03:01 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -11,264 +11,137 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
 
-int length(char *str)
-{
-    int x;
-
-    x = 0;
-    while (str[x] != 0)
-        x++;
-    return (x);
-}
-
-int check_jump(char *save)
-{
-    int x;
-
-    x = 0;
-    if (!save)
-        return (0);
-    while (save[x] != 0)
-    {
-        if (save[x] == '\n')
-            return (x + 1);
-        x++;
-    }
-    return (0);
-}
-
-char    *ft_join(char *save, char *str)
-{
-    int     x;
-    int     y;
-    int     len;
-    char    *aux;
-
-    x = 0;
-    y = 0;
-    len = 0;
-    if (save[0] != 0)
-        len = length(save);
-    len += length(str);
-    aux = malloc(sizeof (char) * len + 1);
-    if (!aux)
-        return (0);
-    while (x != length(save))
-    {
-        aux[x] = save[x];
-        x++;
-    }
-    while (x != len)
-    {
-        aux[x] = str[y];
-        x++;
-        y++;
-    }
-    aux[x] = 0;
-    free(save);
-    return (aux);
-}
-
-char    *get_new_save(char *save)
-{
-    char    *nsave;
-    int     x;
-    int     y;
-
-    x = 0;
-    y = length(save);
-    while (save[x] != '\n')
-        x++;
-    x++;
-    y = y - x;
-    nsave = malloc(sizeof (char) * y + 1);
-    if (!nsave)
-        return (0);
-    y = 0;
-    while (save[x] != 0)
-    {
-        nsave[y] = save[x];
-        y++;
-        x++;
-    }
-    nsave[y] = 0;
-    free(save);
-    return (nsave);
-}
-
-char    *take_line(char *save)
+char    *take_line(char *save, int x)
 {
     char    *line;
-    int     x;
     int     y;
 
-    x = 0;
     y = 0;
-    while (save[y] != '\n')
-        y++;
-    y++;
-    line = malloc(sizeof (char) * y + 1);
+    line = malloc(sizeof(char) * x + 1);
     if (!line)
         return (0);
-    while (x != y)
+    line[x] = 0;
+    while (y < x)
     {
-        line[x] = save[x];
+        line[y] = save[y];
+        y++;
+    }
+    return (line);
+}
+
+char    *get_new_save(char *save, int x)
+{
+    char    *nsave;
+    int     y;
+    int     j;
+
+    y = length(save);
+    j = 0;
+    nsave = malloc(sizeof(char) * (y - x) + 1);
+    if (!nsave)
+        return (0);
+    nsave[y - x] = 0;
+    while (x <= y)
+    {
+        nsave[j] = save[x];
+        j++;
         x++;
     }
-    line[x] = 0;
-    return (line);
+    free(save);
+    return (nsave);
 }
 
 char    *get_no_jump(char *save)
 {
     char    *str;
     int     x;
-    int     y;
 
+    if (save[0] == 0)
+        return (0);
+    x = 0;
     x = length(save);
-    y = 0;
     str = malloc(sizeof (char) * x + 1);
     if (!str)
         return (0);
-    while (y < x)
-    {
-        str[y] = save[y];
-        y++;
-    }
     str[x] = 0;
-    free(save);
+    x = 0;
+    while (str[x] != 0)
+    {
+        str[x] = save[x];
+        x++;
+    }
     return (str);
 }
 
-static char *free_save(char *save)
+char    *comp_save(char *save)
 {
+    int     x;
+
+    if (check_jump(save) != 0)
+    {
+        x = check_jump(save);
+        save = get_new_save(save, x);
+        return (save);
+    }
     free(save);
     return (0);
 }
 
-/*char  *send_ret(char *save, char *str)
+char    *comp_str(char *save)
 {
+    int     x;
+    char    *line;
+
     if (check_jump(save) != 0)
     {
-        str = take_line(save);
-        save = get_new_save(save);
-        return (str);
+        x = check_jump(save);
+        line = take_line(save, x);
+        return (line);
     }
-    else if (save[0] == 0)
-    {
-        free_save(save);
-        return (0);
-    }
-    else
-    {
-        int len;
-
-        len = length(save);
-        str = malloc(sizeof (char) * len + 1);
-        str[len] = 0;
-        len = 0;
-        while (save[len] != 0)
-        {
-            str[len] = save[len];
-            len++;
-        }
-        save = free_save(save);
-        return (str);
-    }
-}*/
+    line = get_no_jump(save);
+    return (line);
+}
 
 char    *get_next_line(int fd)
 {
     static char *save;
     char        *str;
+    char        *line;
     int         buf;
 
+    if (BUFFER_SIZE <= 0)
+        return (0);
     str = malloc(sizeof (char) * BUFFER_SIZE + 1);
     if (!save)
-    {
-        printf("   h o l a         ");
         save = malloc(sizeof (char) * BUFFER_SIZE + 1);
-    }
-    if(!save || BUFFER_SIZE <= 0 || !str)
-        return 0;
+    if (!str || !save)
+        return (0);
     while (check_jump(save) == 0)
     {
         buf = read(fd, str, BUFFER_SIZE);
         if (buf <= 0)
-            break ;
+            break;
         str[buf] = 0;
         save = ft_join(save, str);
     }
     free(str);
-
-    int len;
-
-        len = length(save);
-        str = malloc(sizeof (char) * len + 1);
-        if (!str)
-            return (0);
-        str[len] = 0;
-        len = 0;
-        while (save[len] != 0)
-        {
-            str[len] = save[len];
-            len++;
-        }
-        save = free_save(save);
-        return (str);
-
-    if (check_jump(save) != 0)
-    {
-        str = take_line(save);
-        save = get_new_save(save);
-        return (str);
-    }
-    else
-    {
-        free(save);
-        save = 0;
-    }
-/*        int len;
-
-        if (save[0] == 0)
-        {
-            free(save);
-            return (0);
-        }
-        len = length(save);
-        str = malloc(sizeof (char) * len + 1);
-        if (!str)
-            return (0);
-        str[len] = 0;
-        len = 0;
-        while (save[len] != 0)
-        {
-            str[len] = save[len];
-            len++;
-        }
-        save = free_save(save);
-        return (str);*/
+    line = comp_str(save);
+    save = comp_save(save);
+    return (line);
 }
-
 /*
 int main(void)
 {
     int     fd;
     int     cont;
     char    *str;
-
     cont = 0;
-    fd = open("41_with_nl", O_RDONLY);
+    fd = open("21_no_line.txt", O_RDONLY);
     str = get_next_line(fd);
     while (str)
     {
-        printf("%s", str);
+        printf("%s\n", str);
         free(str);
         str = get_next_line(fd);
     }
@@ -276,5 +149,4 @@ int main(void)
     free(str);
     close(fd);
     return (0);
-    }
-*/
+}*/
